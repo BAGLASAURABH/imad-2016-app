1ede52eca7ed9cbdfb89456a7f49f569ff5d592d
BAGLASAURABH <baglasaurabh6@gmail.com>
2016-11-26 22:26:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').pool;
 
+var config = {
+    user: 'baglasaurabh',
+    database: 'baglasuarabh',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+  // make a select request
+  // return a response with the results
+  pool.query('SELECT * FROM test',function (err, result) {
+     if (err) {
+         res.status(500).send(err.toString());
+     } else {
+         res.send(JSON.stringify(result));
+     }
+  });
+ });
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
